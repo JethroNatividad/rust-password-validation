1e61f3377660809a2ef6563e353129097f2b6c4b
JethroNatividad <natividad.jet@gmail.com>
2024-01-06 23:33:22 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 // Program that has a hard coded password, takes in a password, and if password matches, show "Welcome!", else show "I don't know you!"
 
+// Inputs: password
+// Process: compare
+// Outputs: if match: "Welcome!", else "I don't know you!"
+
 fn main() {
     println!("Hello, world!");
 }
