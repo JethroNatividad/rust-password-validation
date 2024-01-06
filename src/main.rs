b46243f210b7a6cd6048fbea37fe8aed164d0e5f
JethroNatividad <natividad.jet@gmail.com>
2024-01-06 23:32:22 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// Program that has a hard coded password, takes in a password, and if password matches, show "Welcome!", else show "I don't know you!"
+
 fn main() {
     println!("Hello, world!");
 }
