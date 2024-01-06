104a69ae14baa9ccfd1f954db4199bc03e544eeb
JethroNatividad <natividad.jet@gmail.com>
2024-01-07 00:03:26 +0800

    update pseudocode

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,15 @@ fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
     }
 }
 
+
 fn main() {
-    // set a password
+    // Set a list of users
     let password: String = "pass".to_string();
+    // prompt for input_username: "What is the username?"
+        // If not exist in users, ask again.
     // prompt for input_password: "What is the password?"
     let input_password: String = get_input("What is the password? ");
-    // compare input_password and password, if match
+    // compare input_password and user password, if match
         // print "Welcome!"
     // if not match
         // print "I don't know you!"
